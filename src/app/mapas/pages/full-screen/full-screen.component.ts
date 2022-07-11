c0b17816eedcd554a48fb570396937d98d0b0021
Joaquin Nicolas Coelho <jnicolas@shs.local>
2022-07-11 13:54:25 +0200

    first commit

---
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
+
+
+@Component({
+  selector: 'app-full-screen',
+  templateUrl: './full-screen.component.html',
+  styles: [
+    `
+      #mapa {
+        width: 100%;
+        height: 100%;
+      }
+    `
+  ]
+})
+export class FullScreenComponent implements OnInit {
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -68.85871731314352, -32.8728670632323 ],
+      zoom: 14
+    });
+
+  }
+
+}
